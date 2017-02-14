b835ff13668c0841b652d5d8168f61721f0d2c61
Douglas Cunha <douglascunha@outlook.com>
2017-02-14 17:48:46 -0200

    Finalizacao das frases.

--- Frases do Dia/ViewController.swift
+++ Frases do Dia/ViewController.swift
@@ -20,6 +20,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func novaFrase(_ sender: Any) {
+        
+        var frases: [String] = []
+        var numeroAleatorio = arc4random_uniform( 3 )
+        frases.append( "Niguém baterá tão forte quanto a vida. Porém, não se trata de quão forte pode bater, se trata de quão forte pode ser atingido e continuar seguindo em frente. É assim que a vitória é conquistada." )
+        frases.append( "Não importa o quanto você bate, mas sim o quanto aguenta apanhar e continuar. O quanto pode suportar e seguir em frente. É assim que se ganha." )
+        frases.append( "O medo é como o fogo, ele queima você por dentro. Se você controlá-lo Tommy, vai queimar você. Mas se ele o controlar, vai queimar você e tudo ao seu redor" )
+        frases.append( "Talvez eu não possa vencer. Talvez a única coisa que eu possa fazer é levar tudo na cara. Mas para eu vencer ele terá que me matar, e para me matar, ele precisa ter coragem de ficar diante de mim. E para fazer isso, ele deve estar disposto a morrer também" )
+        
+        legendaResultado.text = frases[ Int(numeroAleatorio) ]
+    }
 
 }
 
